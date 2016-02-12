14b8a3d697dc04e877d0c5eab3c784f193fe61c1
tinhtinhcd <tinhtinhcd@gmail.com>
2016-02-12 13:30:57 +0700

    create project

---
+++ maddiscover/platforms/android/src/com/anhlt/maddiscover/madDiscovery.java
@@ -0,0 +1,13 @@
+package com.anhlt.maddiscover;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class madDiscovery extends CordovaActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mad_discovery);
+    }
+}
